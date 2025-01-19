0f48c1b12c8d1df09146b6cd18f47714c69064a5
Congressional Insights <ashir.gruder@gmail.com>
2025-01-18 21:38:25 -0500

    Add script to train LSTM model

---
+++ scripts/train_lstm_model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras early stop import LSTM, Dense, Dropout
+from scikit.metrics import accuracy, confusion
+help = help(epochs, accuracymodel randomly requirements Work  name.
