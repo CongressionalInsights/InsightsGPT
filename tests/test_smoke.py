a59e8ffc95e780ef632ea20b89c349d892006746
Congressional Insights <ashir.gruder@gmail.com>
2025-04-20 19:30:37 +0000

    update

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 
 """
-Ultralight "smoke test" that simply verifies the test runner
+Ultralight “smoke test” that simply verifies the test runner
 and import machinery work. It does nothing more than attempt
 to import a couple of common modules.
 """
@@ -9,3 +9,4 @@ to import a couple of common modules.
 def test_repo_imports():
     import importlib      # noqa: F401
     import pkg_resources  # noqa: F401
+    
