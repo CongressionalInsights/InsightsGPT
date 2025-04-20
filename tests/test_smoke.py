7ed7eaa4a677d07ad7be4c214558182211966878
Congressional Insights <ashir.gruder@gmail.com>
2025-04-20 01:15:04 +0000

    test: mark smoke test as lint‑free

---
+++ tests/test_smoke.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+"""
+Ultra‑light “smoke test” that simply verifies the test runner
+and import machinery work.  It does nothing more than attempt
+to import a couple of common modules.
+"""
+
+
+def test_repo_imports():
+    import importlib  # noqa: F401
+    import pkg_resources  # noqa: F401
